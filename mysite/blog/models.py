244fbde3fa5122197f058688bb6da38d18ce3597
Cooper Parlee <cparlee9@gmail.com>
2021-03-30 14:08:20 -0400

    Restyled post list section

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,6 +11,7 @@ class Post (models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=200)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
