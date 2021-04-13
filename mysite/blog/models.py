d20f3a5b9ab2685745f7f1fa67a66252c994c2f3
Cooper Parlee <cooper@cooperparlee.com>
2021-04-12 21:23:31 -0400

    Programmatically added (or didnt) post header images

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -18,6 +18,7 @@ CATEGORIES = (
 
 class Post (models.Model):
     title = models.CharField(max_length=200, unique=True)
+    header_image = models.ImageField(null=True, blank=True, upload_to="media/images/")
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
