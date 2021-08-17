a7300fcfe72e1fce1514ea3cac07813537278f31
Cooper Parlee <cooper@cooperparlee.com>
2021-08-17 14:17:30 +0000

    Moved media root out of static; prevented errors

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_assets'),
 )
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
